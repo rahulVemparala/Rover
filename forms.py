fd3a441b72c92bd29f5ab4647527030557f2feb7
rahul <vrready4u@gmail.com>
2023-04-27 02:27:08 -0400

    register, login and validations

---
+++ forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[
+                           DataRequired(), Length(2, 20)])
+    email = StringField('email', validators=[
+        DataRequired(), Email()])
+    password = StringField('password', validators=[
+        DataRequired()])
+    confirm_password = StringField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password')])
+    submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[
+        DataRequired(), Email()])
+    password = StringField('password', validators=[
+        DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -12,7 +12,23 @@ Endpoints:
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+# setting this to prevent from modifying cookies, site attacks during register/login page
+# Secretkey :  produced using secrets lib, random num
+app.config['SECRET_KEY'] = "ea8af9fdf1f57149062f3985e62114b4"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///site.db'
+
+
+db = SQLAlchemy(app=app)
+
+# tables in SQLAlchemy are structured as classes in python , to work with them with ease
+
 
 posts = [
     {
@@ -29,10 +45,9 @@ posts = [
 
 ]
 
-app = Flask(__name__)
-
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
@@ -40,6 +55,30 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/signout")
+def signout():
+    return render_template('signout.html', title='About')
+
+
+@app.route("/register", methods=['GET', "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(
+            message=f"Account created for {form.username.data}", category='success')
+        return redirect(url_for('home'))
+    return render_template('register2.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+
+    return render_template('login.html', title='Register', form=form)
 
 
 if __name__ == "__main__":
