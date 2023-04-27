81fa0723a311acc522d0a2017014f014b31078e3
rahul <vrready4u@gmail.com>
2023-04-27 13:25:27 -0400

    conditional navbar

--- rover/database.py
+++ rover/database.py
@@ -3,9 +3,15 @@
 ORM files, or classes that are similar to db tables
 """
 from rover import db
+from rover import login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
